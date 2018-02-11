8a34b7405dfa5184af4ca02efbec97ed6a731b28
bpor92 <bartoszporebski1992@gmail.com>
2018-02-11 18:43:56 +0100

    add Router and Store

---
+++ src/Layout/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">Home</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <router-link to="/" tag="li" class="nav-item" active-class="active" exact>
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link to="/user" tag="li" class="nav-item" active-class="active">
+          <a class="nav-link">User</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import { store } from './Store/store'
+import { routes } from './Router/routes'
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   el: '#app',
+  store,
+  router,
   render: h => h(App)
 })
