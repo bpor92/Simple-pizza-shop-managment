838765747eef113bca60a4f9e7c0c2ac3e2a7337
bpor92 <bartoszporebski1992@gmail.com>
2018-02-24 10:39:41 +0100

    veeValidate init

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -6,6 +6,14 @@
         <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa quis laborum voluptatem facere, non, perferendis excepturi ab aliquid velit distinctio, similique incidunt tenetur necessitatibus facilis. Cumque ullam consequatur voluptates in.</p>
       </div>
     </div>
+        <input v-validate="'required|email'" class="form-control" :class="{'is-invalid': errors.has('emails') }" name="emails" type="text" placeholder="Email">
+        <div v-show="errors.has('emails')"  class="invalid-feedback">{{ errors.first('emails') }}</div>
+        
+        <p class="jumbotron">
+          <pre>
+            {{errors}}
 
+          </pre>
+        </p>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import VeeValidate from 'vee-validate'
 
 import App from './App.vue'
 
@@ -9,6 +10,8 @@ import { routes } from './Router/routes'
 
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
+
 const router = new VueRouter({
   mode: 'history',
   routes
